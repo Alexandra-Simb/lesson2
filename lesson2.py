21ed71013856a2801fd7af760d1af3a9b059318d
Alexandra-Simb <simbirskaya.asya@mail.ru>
2024-08-14 19:58:39 +0300

    Add files via upload

---
+++ lesson2.py
@@ -0,0 +1,5 @@
+amount_of_homework = 12
+number_of_hours_spent = 1.5
+course_name = 'Python'
+time_for_one_task = (1.5 / 12)
+print('курс'':',course_name,','  'всего задач'':', amount_of_homework, 'затрачено часов'':', number_of_hours_spent, 'среднее время выполнения'':',time_for_one_task)
